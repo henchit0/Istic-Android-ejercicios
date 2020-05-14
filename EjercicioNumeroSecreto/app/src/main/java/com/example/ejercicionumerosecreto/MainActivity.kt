cbb294882effc704d0ab5e32ada63cc885d41c33
henchit0 <ezequiel.yeye.rosso@gmail.com>
2020-05-13 22:35:12 -0300

    Numero secreto cuasi terminado

--- EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/MainActivity.kt
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ejercicionumerosecreto
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -7,29 +8,16 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        btnComenzar.setOnClickListener{
-            txtMensaje.text = (Math.random() * 100).toInt().toString();
-
-        }
-
-        btnAdivinar.setOnClickListener{
-
-            var numeroIngresado = txtIngresar.toString().toInt();
-            if (numeroIngresado == txtMensaje.toString().toInt())
-            {
-                Toast.makeText(this, "Sos un crack", Toast.LENGTH_SHORT).show()
-            }
-        }
-        /*var contador:Int = 0;
-        btnLogin.setOnClickListener{
-            contador ++;
-            txtMostrar.text = contador.toString();
-        }*/
+        var numeroSecretoIntent = Intent(this, NumeroSecreto::class.java)
+        startActivity(numeroSecretoIntent)
 
     }
+
+
 }
--- EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/NumeroSecreto.kt
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/NumeroSecreto.kt
@@ -2,11 +2,66 @@ package com.example.ejercicionumerosecreto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_numero_secreto.*
 
 class NumeroSecreto : AppCompatActivity() {
+
+    var numeroSecreto = 0
+    var contadorVidas = 5
+    var contadorIntentos = 0
+
+    fun Iniciar(){
+        numeroSecreto = (Math.random() * 100).toInt();
+        contadorVidas = 5
+        contadorIntentos = 0
+        //numeroSecreto = 4;//(Math.random() * 100).toInt();
+        txtMensaje.text = "Adivina!";
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numero_secreto)
+
+        btnComenzar.setOnClickListener{
+            this.Iniciar();
+        }
+
+        btnAdivinar.setOnClickListener{
+            if (txtIngresar.text.toString().toInt() > 100){
+                Toast.makeText(this, "El numero tiene que ser menor a 100.", Toast.LENGTH_LONG).show()
+                txtIngresar.clearComposingText()
+            }
+            else if (txtIngresar.text.toString().toInt() < 1)
+            {
+                Toast.makeText(this, "El numero tiene que ser mayor a 0.", Toast.LENGTH_LONG).show()
+            }
+            else if (contadorVidas > 0)
+            {
+                if (txtIngresar.text.toString().toInt() < numeroSecreto)
+                {
+                    Toast.makeText(this, "Mandale mas.", Toast.LENGTH_LONG).show()
+                    contadorVidas --
+                    contadorIntentos ++
+                }
+                else if (txtIngresar.text.toString().toInt() > numeroSecreto)
+                {
+                    Toast.makeText(this, "Te pasaste un toque.", Toast.LENGTH_LONG).show()
+                    contadorVidas --
+                    contadorIntentos ++
+
+                }
+                else if (txtIngresar.text.toString().toInt() == numeroSecreto)
+                {
+                    Toast.makeText(this, "Adivinaste!!!", Toast.LENGTH_LONG).show()
+                    contadorVidas = 5
+                }
+            }
+            else if (contadorVidas == 0)
+            {
+                Toast.makeText(this, "Perdiste!!! :( El numero era " + numeroSecreto, Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
