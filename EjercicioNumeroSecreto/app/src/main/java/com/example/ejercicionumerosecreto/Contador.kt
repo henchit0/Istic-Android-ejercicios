2793609775625c6781e074e46ab2d51ea50da1eb
henchit0 <ezequiel.yeye.rosso@gmail.com>
2020-05-13 21:09:11 -0300

    Se agrego intent

---
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/Contador.kt
@@ -0,0 +1,12 @@
+package com.example.ejercicionumerosecreto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Contador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contador)
+    }
+}
---
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/NumeroSecreto.kt
@@ -0,0 +1,12 @@
+package com.example.ejercicionumerosecreto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NumeroSecreto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numero_secreto)
+    }
+}
