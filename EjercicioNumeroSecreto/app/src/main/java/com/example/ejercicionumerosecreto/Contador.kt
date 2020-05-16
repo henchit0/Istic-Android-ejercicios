a5f8798d259e97ae7c746644ea2acc234dd9aad9
henchit0 <ezequiel.yeye.rosso@gmail.com>
2020-05-16 18:56:30 -0300

    Se agregan gestos para navegar pantallas

--- EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/Contador.kt
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/Contador.kt
@@ -1,12 +1,112 @@
 package com.example.ejercicionumerosecreto
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 
 class Contador : AppCompatActivity() {
+
+    private lateinit var  detector : GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contador)
+
+        detector = GestureDetectorCompat(this, GestosComunes())
+
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (detector.onTouchEvent(event))
+        {
+            true
+        }
+        else
+        {
+            super.onTouchEvent(event)
+        }
+
+    }
+
+    inner class GestosComunes : GestureDetector.SimpleOnGestureListener()
+    {
+
+        private val LIMETE_SWIPE = 100
+        private val LIMETE_SWIPE_VELOCIDAD = 100
+
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if (Math.abs(diffX) > Math.abs(diffY))
+            {       // Esto seria un swipe de derecha a izq o viceversa
+                if (Math.abs(diffX) > LIMETE_SWIPE && Math.abs(velocityX) > LIMETE_SWIPE_VELOCIDAD)
+                {
+                    if (diffX > 0)
+                    {       // Swipe derecha a izquierda
+                        this@Contador.onSwipeDerecha()
+                    }
+                    else
+                    {       //swipe izquieda a derecha
+                        this@Contador.onSwipeIzquierda()
+                    }
+                    true
+                }
+                else
+                {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+            else
+            {       // Esto seria un swioe de arrriba a abajo o viceversa
+                if (Math.abs(diffY) > LIMETE_SWIPE && Math.abs(velocityY) > LIMETE_SWIPE_VELOCIDAD)
+                {
+                    if (diffY > 0)
+                    {       // Swipe derecha a izquierda
+                        this@Contador.onSwipeAbajo()
+                    }
+                    else
+                    {       //swipe izquieda a derecha
+                        this@Contador.onSwipeArriba()
+                    }
+                    true
+                }
+                else
+                {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+    }
+
+    private fun onSwipeArriba() {
+        Toast.makeText(this,"Swipe arriba", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeAbajo() {
+        Toast.makeText(this,"Swipe abajo", Toast.LENGTH_LONG).show()    }
+
+    private fun onSwipeIzquierda() {
+        //Toast.makeText(this,"Swipe izquierda",Toast.LENGTH_LONG).show()
+        var mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
+    }
+
+    private fun onSwipeDerecha() {
+        //Toast.makeText(this,"Swipe derecha",Toast.LENGTH_LONG).show()
+        var contadorIntent = Intent(this, Contador::class.java)
+        startActivity(contadorIntent)
+    }
+
+
+
 }
--- EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/NumeroSecreto.kt
+++ EjercicioNumeroSecreto/app/src/main/java/com/example/ejercicionumerosecreto/NumeroSecreto.kt
@@ -1,7 +1,11 @@
 package com.example.ejercicionumerosecreto
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import android.widget.TextView
@@ -12,6 +16,7 @@ class NumeroSecreto : AppCompatActivity() {
     var numeroSecreto = 0
     var contadorVidas = 5
     var contadorIntentos = 0
+    private lateinit var  detector : GestureDetectorCompat
 
     fun Iniciar(){
         numeroSecreto = (Math.random() * 100).toInt();
@@ -23,12 +28,15 @@ class NumeroSecreto : AppCompatActivity() {
         txtIngresar.setHint("Ingresa tu numero!")
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numero_secreto)
 
+        detector = GestureDetectorCompat(this, GestosComunes())
+
         btnComenzar.setOnClickListener{
-            this.Iniciar();
+            this.Iniciar()
         }
 
         btnAdivinar.setOnClickListener{
@@ -66,6 +74,92 @@ class NumeroSecreto : AppCompatActivity() {
                 Toast.makeText(this, "Perdiste!!! :( El numero era " + numeroSecreto, Toast.LENGTH_LONG).show()
             }
         }
+    }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (detector.onTouchEvent(event))
+        {
+            true
+        }
+        else
+        {
+            super.onTouchEvent(event)
+        }
+
+    }
+
+    inner class GestosComunes : GestureDetector.SimpleOnGestureListener()
+    {
+
+        private val LIMETE_SWIPE = 100
+        private val LIMETE_SWIPE_VELOCIDAD = 100
+
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if (Math.abs(diffX) > Math.abs(diffY))
+            {       // Esto seria un swipe de derecha a izq o viceversa
+                if (Math.abs(diffX) > LIMETE_SWIPE && Math.abs(velocityX) > LIMETE_SWIPE_VELOCIDAD)
+                {
+                    if (diffX > 0)
+                    {       // Swipe derecha a izquierda
+                        this@NumeroSecreto.onSwipeDerecha()
+                    }
+                    else
+                    {       //swipe izquieda a derecha
+                        this@NumeroSecreto.onSwipeIzquierda()
+                    }
+                    true
+                }
+                else
+                {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+            else
+            {       // Esto seria un swioe de arrriba a abajo o viceversa
+                if (Math.abs(diffY) > LIMETE_SWIPE && Math.abs(velocityY) > LIMETE_SWIPE_VELOCIDAD)
+                {
+                    if (diffY > 0)
+                    {       // Swipe derecha a izquierda
+                        this@NumeroSecreto.onSwipeAbajo()
+                    }
+                    else
+                    {       //swipe izquieda a derecha
+                        this@NumeroSecreto.onSwipeArriba()
+                    }
+                    true
+                }
+                else
+                {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+    }
+
+    private fun onSwipeArriba() {
+        Toast.makeText(this,"Swipe arriba",Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeAbajo() {
+        Toast.makeText(this,"Swipe abajo",Toast.LENGTH_LONG).show()    }
+
+    private fun onSwipeIzquierda() {
+        //Toast.makeText(this,"Swipe izquierda",Toast.LENGTH_LONG).show()
+        var mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
+    }
+
+    private fun onSwipeDerecha() {
+        //Toast.makeText(this,"Swipe derecha",Toast.LENGTH_LONG).show()
+        var contadorIntent = Intent(this, Contador::class.java)
+        startActivity(contadorIntent)
     }
 }
